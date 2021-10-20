037f6b51120db46ef71190728c26d2bc080a747e
Adrian Dumitru Grigoriu <adrian.grigoriu@student.usv.ro>
2021-10-20 23:42:02 +0300

    L2

---
+++ Grigoriu/Program.cs
@@ -0,0 +1,167 @@
+﻿/*
+ Grigoriu Adrian
+ Grupa 3132B
+ Laborator 2
+ */
+
+
+using System;
+using System.Drawing;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
+namespace Grigoriu
+{
+    class SimpleWindow3D : GameWindow
+    {
+        const float rotation_speed = 90.0f;
+        float angle;
+        bool showCube = true;
+        KeyboardState lastKeyPress;
+       
+
+        public SimpleWindow3D() : base(800, 600)
+        {
+            VSync = VSyncMode.On;
+        }
+
+        //Culoare fundal
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            GL.ClearColor(Color.GreenYellow);
+           
+        }
+
+        //Inițierea afișării și setarea viewport-ului
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            GL.Viewport(0, 0, Width, Height);
+
+            double aspect_ratio = Width / (double)Height;
+
+            Matrix4 perspective = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, (float)aspect_ratio, 1, 64);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref perspective);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+            Mouse.GetCursorState();
+            KeyboardState keyboard = OpenTK.Input.Keyboard.GetState();
+            MouseState mouse = OpenTK.Input.Mouse.GetState();
+
+            if (keyboard[OpenTK.Input.Key.Escape])
+            {
+                Exit();
+                return;
+            }
+            else if (keyboard[OpenTK.Input.Key.P] && !keyboard.Equals(lastKeyPress))
+            {
+                // Ascundere comandată, prin apăsarea unei taste
+                if (showCube == true)
+                {
+                    showCube = false;
+                }
+                else
+                {
+                    showCube = true;
+                }
+            }
+            lastKeyPress = keyboard;
+
+            if (mouse[OpenTK.Input.MouseButton.Left])
+            {
+                // Ascundere comandată, prin clic de mouse
+                if (showCube == true)
+                {
+                    showCube = false;
+                }
+                else
+                {
+                    showCube = true;
+                }
+            }
+        }
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            Matrix4 lookat = Matrix4.LookAt(15, 50, 15, 0, 0, 0, 0, 1, 0);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref lookat);
+
+            angle += rotation_speed * (float)e.Time;
+            GL.Rotate(angle, 0.0f, 1.0f, 0.0f);
+
+            if (showCube == true)
+            {
+                DrawCube();
+            }
+
+            SwapBuffers();
+        }
+
+        private void DrawCube()
+        {
+            GL.Begin(PrimitiveType.Quads);
+
+            GL.Color3(Color.Silver);
+            GL.Vertex3(-1.0f, -1.0f, -1.0f);
+            GL.Vertex3(-1.0f, 1.0f, -1.0f);
+            GL.Vertex3(1.0f, 1.0f, -1.0f);
+            GL.Vertex3(1.0f, -1.0f, -1.0f);
+
+            GL.Color3(Color.Honeydew);
+            GL.Vertex3(-1.0f, -1.0f, -1.0f);
+            GL.Vertex3(1.0f, -1.0f, -1.0f);
+            GL.Vertex3(1.0f, -1.0f, 1.0f);
+            GL.Vertex3(-1.0f, -1.0f, 1.0f);
+
+            GL.Color3(Color.Moccasin);
+            GL.Vertex3(-1.0f, -1.0f, -1.0f);
+            GL.Vertex3(-1.0f, -1.0f, 1.0f);
+            GL.Vertex3(-1.0f, 1.0f, 1.0f);
+            GL.Vertex3(-1.0f, 1.0f, -1.0f);
+
+            GL.Color3(Color.IndianRed);
+            GL.Vertex3(-1.0f, -1.0f, 1.0f);
+            GL.Vertex3(1.0f, -1.0f, 1.0f);
+            GL.Vertex3(1.0f, 1.0f, 1.0f);
+            GL.Vertex3(-1.0f, 1.0f, 1.0f);
+
+            GL.Color3(Color.PaleVioletRed);
+            GL.Vertex3(-1.0f, 1.0f, -1.0f);
+            GL.Vertex3(-1.0f, 1.0f, 1.0f);
+            GL.Vertex3(1.0f, 1.0f, 1.0f);
+            GL.Vertex3(1.0f, 1.0f, -1.0f);
+
+            GL.Color3(Color.Blue);
+            GL.Vertex3(1.0f, -1.0f, -1.0f);
+            GL.Vertex3(1.0f, 1.0f, -1.0f);
+            GL.Vertex3(1.0f, 1.0f, 1.0f);
+            GL.Vertex3(1.0f, -1.0f, 1.0f);
+
+            GL.End();
+        }
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            using (SimpleWindow3D cub = new SimpleWindow3D())
+            {
+                cub.Run(30.0, 0.0);
+            }
+        }
+
+
+
+    }
+}
